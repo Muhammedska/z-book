33a5a63fb14f6edbc3eea9cc575795162bc10d4e
SafetyZone <81029510+Muhammedska@users.noreply.github.com>
2023-06-19 22:03:44 +0300

    update

--- pages/system.js
+++ pages/system.js
@@ -3,24 +3,27 @@ const pageMode = document.getElementById('pgmd');
 
 let pageType = 'A4';
 let zoomvl = 100;
-pageMode.onclick =()=>{
+let isScroll = 0;
+document.getElementById('wrapper').classList.add('toggled')
+
+pageMode.onclick = () => {
   console.log(pageType)
-  if(pageType == 'fill'){
+  if (pageType == 'fill') {
     pageType = 'A4'
     let allItem = document.querySelectorAll('page')
     for (const element of allItem) {
       element.classList.remove('fill');
       element.classList.add('A4');
     }
-     
-   
-  }else if(pageType == 'A4'){
+
+
+  } else if (pageType == 'A4') {
     pageType = 'fill'
     let allItem = document.querySelectorAll('page')
     for (const element of allItem) {
       element.classList.add('fill');
       element.classList.remove('A4');
-    } 
+    }
   }
   pageMode.innerText = pageType.toUpperCase();
 }
@@ -67,23 +70,38 @@ function dragElement(elmnt) {
     document.onmousemove = null;
   }
 }
-window.onscroll = function() {
+window.onscroll = function () {
   console.log("scrolled  " + window.pageYOffset)
+  var locate = document.getElementById("mydiv").offsetTop ;
+  if(isScroll > window.pageYOffset){
+    var calc = isScroll - window.pageYOffset
+    document.getElementById("mydiv").style.top = locate - calc +"px";
+  }else if(isScroll < window.pageYOffset){
+    var calc = isScroll - window.pageYOffset
+    document.getElementById("mydiv").style.top = locate - calc +"px";
+
+  }
+  isScroll = window.pageYOffset;
+  console.log("scrolled  " + window.pageYOffset +"px / new location  " + document.getElementById("mydiv").offsetTop )
 };
 
-function zoomplus(){
+function zoomplus() {
   zoomvl += 10
   var page = document.getElementsByTagName('page');
-  var zoom = parseInt(page.style.zoom)+ 10 + '%';
-  page.style.zoom = zoomvl
-  document.getElementById('zoomval').innerText = "%"+zoomvl;
+  for (const iterator of page) {
+
+    iterator.style.zoom = zoomvl+"%";
+  }
+  document.getElementById('zoomval').innerText = "%" + zoomvl;
 }
 
-function zoomminus(){
+function zoomminus() {
   zoomvl -= 10
-  var page = document.getElementsByTagName('page');
-  var zoom = parseInt(page.style.zoom)+ 10 + '%';
-  page.style.zoom = zoomvl
-  document.getElementById('zoomval').innerText = "%"+zoomvl;
+  var page = document.querySelectorAll('page');
+  for (const iterator of page) {
+
+    iterator.style.zoom = zoomvl+"%";
+  }
+  document.getElementById('zoomval').innerText = "%" + zoomvl;
 }
 
